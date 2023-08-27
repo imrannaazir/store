c08ab4c7530f5b9c2b5067615823db429c6a784b
emon <imrannaaziremon@gmail.com>
2023-08-27 13:11:52 +0600

    added gallery

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -1,5 +1,36 @@
+import { useDispatch } from "react-redux";
+import Modal from "./ui/modal";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { closeModal } from "@/redux/features/modalSlice";
+import Gallery from "./gallery";
+
 const PreviewModal = () => {
-  return <div>Preview Modal</div>;
+  const dispatch = useDispatch<AppDispatch>();
+  const { isOpen, data } = useAppSelector((state) => state.previewModal);
+
+  if (!data) {
+    return null;
+  }
+  return (
+    <Modal isOpen={isOpen} onClose={() => dispatch(closeModal())}>
+      <div
+        className="
+                grid
+                w-full
+                grid-cols-1
+                items-center
+                gap-x-6
+                gap-y-8
+                sm:grid-cols-12
+                lg:gap-x-8
+      "
+      >
+        {/* gallery */}
+        <Gallery images={data.images} />
+        {/* info */}
+      </div>
+    </Modal>
+  );
 };
 
 export default PreviewModal;
